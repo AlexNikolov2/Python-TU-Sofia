313a0f0cb040e14afbd469a6a9c81a9b7fff972b
Aleks Nikolov <tcr.official.world@gmail.com>
2023-11-14 18:09:29 +0200

    blablabla

--- laboratory-exercise-4/ex2.py
+++ laboratory-exercise-4/ex2.py
@@ -1,16 +1,11 @@
 list_name = []
+list_age = []
 
 size_name = int(input('set size'))
 
 for name in range(size_name):
     name = input('set name:')
     list_name.append(name)
-
-list_age = []
-
-size_age = size_name
-
-for age in range(size_age):
     age = int(input('set age:'))
     list_age.append(age)
 
