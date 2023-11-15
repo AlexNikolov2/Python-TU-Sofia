5253c1a892a46bdf4b4ebe2c89e45bb8386c762c
Aleks Nikolov <tcr.official.world@gmail.com>
2023-11-15 17:19:19 +0200

    tuples ez

---
+++ seminary-exercise-1/ex7.py
@@ -0,0 +1,8 @@
+tuple = (1,2,3,4,5,6)
+
+sum = 0
+
+for i in tuple:
+    sum += i
+
+print(sum)
---
+++ seminary-exercise-1/ex8.py
@@ -0,0 +1,8 @@
+tuple = ('ivan', 'petkan', 'gosho')
+
+el = input('set element')
+
+if(el in str(tuple)):
+    print(True)
+else:
+    print(False)
