d74f7db41d626e95ef8ecc2839a438a6b379c65b
Aleks Nikolov <tcr.official.world@gmail.com>
2023-11-15 17:44:43 +0200

    ez clap

---
+++ seminary-exercise-1/ex11.py
@@ -0,0 +1,11 @@
+def square(a):
+    s = a*a
+    print(s)
+def rectangle(a,b):
+    s = a*b
+    print(s)
+def triangle(a,b):
+    s = (a*b)/2
+    print(s)
+
+square(5), rectangle(10,20), triangle(10,20)
