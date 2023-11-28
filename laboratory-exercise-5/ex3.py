3d878dc1b7bd72b6b08f28f376b2808527831ab4
Aleks Nikolov <tcr.official.world@gmail.com>
2023-11-28 17:19:12 +0200

    lol

--- laboratory-exercise-5/ex3.py
+++ laboratory-exercise-5/ex3.py
@@ -1,5 +1,7 @@
 def generate_phone_book(names, numbers):
     phone_book = dict(zip(names, numbers))
+    for name, number in phone_book.items():
+        phone_book[name] = number
     
     return phone_book
 
@@ -7,5 +9,17 @@ def delete_phone_book(phone_book, name):
     del phone_book[name]
 
 def print_phone_book(phone_book):
-    #print
-    print_phone_book()
+    for name, number in phone_book.items():
+        print(f"name: {name}, number: {number}")
+
+size = int(input('set size'))
+names = []
+numbers = []
+
+for i in range(size):
+    name = input('set name')
+    names.append(name)
+    number = input('set number')
+    numbers.append(number)
+phone_book = generate_phone_book(names, numbers)
+print_phone_book(phone_book)
