a0602f0e166f8331b0d8eed6d802591578f36f19
Aleks Nikolov <tcr.official.world@gmail.com>
2023-12-20 11:51:37 +0200

    .

---
+++ laboratory-exercise-9/ex2.py
@@ -0,0 +1,15 @@
+class Router:
+    def __init__(self, manufacturer, code, price, amount):
+        self.manufacturer = manufacturer
+        self.code = code
+        self.price = price
+        self.amount = amount
+def main():
+    manufacturer = input('manufacturer ')
+    code = input('code ')
+    price = int(input('price '))
+    amount = int(input('amount '))
+    
+#make method to add more products
+#make method to show final full price with VAT and delivery price
+        
